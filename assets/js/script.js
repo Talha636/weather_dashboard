d1abc5254128f6772f609021449c5fd4fa0ed3c0
Talha636 <85548877+Talha636@users.noreply.github.com>
2021-09-29 01:08:59 -0400

    cards complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,6 +27,10 @@ search.addEventListener('click', function() {
         .then(function(info) {
             console.log(info);
             this.showCurrentInfo(info);
+            this.futureIcon(info);
+            this.futureTemp(info);
+            this.futureWind(info);
+            this.futureHumidity(info);
         })
     })
     
@@ -51,3 +55,40 @@ function futureDate() {
     document.getElementById('future5').innerText = moment().add(5, 'd').format('MMMM Do, YYYY');
 }
 futureDate();
+
+function futureIcon(info) {
+    let icon1 = info.daily[0].weather[0].icon;
+    let icon2 = info.daily[1].weather[0].icon;
+    let icon3 = info.daily[2].weather[0].icon;
+    let icon4 = info.daily[3].weather[0].icon;
+    let icon5 = info.daily[4].weather[0].icon;
+    document.querySelector('#icon1').src = 'http://openweathermap.org/img/wn/' + icon1 + '.png';
+    document.querySelector('#icon2').src = 'http://openweathermap.org/img/wn/' + icon2 + '.png';
+    document.querySelector('#icon3').src = 'http://openweathermap.org/img/wn/' + icon3 + '.png';
+    document.querySelector('#icon4').src = 'http://openweathermap.org/img/wn/' + icon4 + '.png';
+    document.querySelector('#icon5').src = 'http://openweathermap.org/img/wn/' + icon5 + '.png';
+}
+
+function futureTemp(info) {
+    document.querySelector('#temp1').innerHTML = info.daily[0].temp.day;
+    document.querySelector('#temp2').innerHTML = info.daily[1].temp.day;
+    document.querySelector('#temp3').innerHTML = info.daily[2].temp.day;
+    document.querySelector('#temp4').innerHTML = info.daily[3].temp.day;
+    document.querySelector('#temp5').innerHTML = info.daily[4].temp.day;
+}
+
+function futureWind(info) {
+    document.querySelector('#wind1').innerHTML = info.daily[0].wind_speed;
+    document.querySelector('#wind2').innerHTML = info.daily[1].wind_speed;
+    document.querySelector('#wind3').innerHTML = info.daily[2].wind_speed;
+    document.querySelector('#wind4').innerHTML = info.daily[3].wind_speed;
+    document.querySelector('#wind5').innerHTML = info.daily[4].wind_speed;
+}
+
+function futureHumidity(info) {
+    document.querySelector('#humidity1').innerHTML = info.daily[0].humidity;
+    document.querySelector('#humidity2').innerHTML = info.daily[1].humidity;
+    document.querySelector('#humidity3').innerHTML = info.daily[2].humidity;
+    document.querySelector('#humidity4').innerHTML = info.daily[3].humidity;
+    document.querySelector('#humidity5').innerHTML = info.daily[4].humidity;
+}
